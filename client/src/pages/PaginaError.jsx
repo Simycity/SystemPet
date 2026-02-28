07e0deacd53a10ba3c2ed6b1ac4e177f708ca121
Simycity <vhzfilho@hotmail.com>
2026-02-28 18:43:57 -0300

    Adicionando código de erro de página

---
+++ client/src/pages/PaginaError.jsx
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap'
+
+// Importando Navigate para navegação entre páginas
+import { useNavigate } from "react-router-dom";
+
+// Código da página
+const PaginaError = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default PaginaError;
