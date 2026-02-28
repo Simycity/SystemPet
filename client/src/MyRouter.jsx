89666a238c960c1b1e174cbdf2b4361444c29cdb
Simycity <vhzfilho@hotmail.com>
2026-02-28 18:39:55 -0300

    Adicionando rotas, página erro e rotas protegidas

---
+++ client/src/MyRouter.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// Importação de páginas principais
+import App from './App.jsx';
+
+
+const router = createBrowserRouter([
+
+])
+
+export default router;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 // Importando Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importando Provedor de Rotas
+import { RouterProvider } from "react-router-dom";
+// Importando a Rota provedora (rota Pai)
+import MyRouter from './MyRouter.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={MyRouter} />
   </StrictMode>,
 )
