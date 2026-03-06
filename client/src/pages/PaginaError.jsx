ab385118b654401f017b7969d7981b2eac1f7371
Simycity <vhzfilho@hotmail.com>
2026-03-06 20:31:20 -0300

    Atualizando códigos de Erro de Página e de Rotas Protegidas.

--- client/src/pages/PaginaError.jsx
+++ client/src/pages/PaginaError.jsx
@@ -1,13 +1,24 @@
-import Button from 'react-bootstrap'
+import Button from "react-bootstrap";
 
 // Importando Navigate para navegação entre páginas
 import { useNavigate } from "react-router-dom";
 
 // Código da página
 const PaginaError = () => {
-    return(
-        <div></div>
-    )
-}
+  const navigate = useNavigate(); //Declarando a navegação entre páginas
+  return (
+    <div>
+      <h2>404: Página não encontrada ou não existe</h2>
+      <Button
+        variant="warning"
+        onClcik={() => {
+          navigate("/home");
+        }}
+      >
+        Voltar para home
+      </Button>
+    </div>
+  );
+};
 
 export default PaginaError;
---
+++ client/src/pages/RotasProtegidas.jsx
@@ -0,0 +1,11 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+import Container from "react-bootstrap/Container";
+
+const RotasProtegidas = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default RotasProtegidas;
