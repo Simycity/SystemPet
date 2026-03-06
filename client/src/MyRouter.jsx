1a8710e0a570ea6d88971e4f2bb570a30db514bd
Simycity <vhzfilho@hotmail.com>
2026-03-06 20:36:19 -0300

    Atualizando MyRouter

--- client/src/MyRouter.jsx
+++ client/src/MyRouter.jsx
@@ -3,6 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 // Importação de páginas principais
 import App from './App.jsx';
 
+// Importação de Erro de Página & Rotas Protegidas
+import PaginaErro from "./pages/PaginaError.jsx";
+// import RotasProtegidas from "./pages/RotasProtegidas.jsx";
 
 const router = createBrowserRouter([
 
