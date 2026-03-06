bd6e2c7761d686f6eba8117f30c8846efc5a13e7
Simycity <vhzfilho@hotmail.com>
2026-03-06 20:43:35 -0300

    Criando Contextos

---
+++ client/src/contexts/UserContext.jsx
@@ -0,0 +1,13 @@
+// Importando os hooks de criação de contexto
+import { createContext, useState, useEffect } from "react";
+
+// Criando Contexto de autorização
+export const AuthContext = createContext();
+
+/*
+    Criando o provider do contexto
+    Provider = fornece as informações para os componentes filhos
+*/
+export const AuthProvider = () => {
+    
+}
