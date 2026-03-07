bd3fd2e0e76c4bbe3aadfa8583dea35be304ffb1
Simycity <vhzfilho@hotmail.com>
2026-03-06 21:02:07 -0300

    Atualizando Login

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -9,16 +9,29 @@ import {
   Alert,
 } from "react-bootstrap";
 
+// Importando pacote de formulário
+import { useForm } from "react-hook-form";
+
+// Importação da verificação de login (autenticação)
+import { AuthContext } from "../../contexts/UserContext.jsx";
+import { useContext } from "react";
+
+// Importando componentes que tratam de vaiáveis
+import { useState, useEffect } from "react";
+
+// Importação do Navigate
+import { Navigate } from "react-router-dom";
+
 // Iniciando o formulário de Login
 const Login = () => {
-    return(
-        <div>
-            <Container>
-                <h1>Login</h1>
-            </Container>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Container>
+        <h1>Login</h1>
+      </Container>
+    </div>
+  );
+};
 
 // Exportando arquivo de Login
 export default Login;
