5fadccde1a2a1f3c90aa32b7d10d711763c64259
Simycity <vhzfilho@hotmail.com>
2026-03-13 20:50:29 -0300

    Atualizando MyRouter e adicionando RotasProtegidas

--- client/src/MyRouter.jsx
+++ client/src/MyRouter.jsx
@@ -1,11 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 
-// Importação de páginas principais
+// Importação do componente principal
 import App from './App.jsx';
 
 // Importação de Erro de Página & Rotas Protegidas
 import PaginaErro from "./pages/PaginaError.jsx";
-// import RotasProtegidas from "./pages/RotasProtegidas.jsx";
+import RotasProtegidas from "./pages/RotasProtegidas.jsx";
+
+// Importação das páginas
+
 
 const router = createBrowserRouter([
 
--- client/src/pages/RotasProtegidas.jsx
+++ client/src/pages/RotasProtegidas.jsx
@@ -2,10 +2,37 @@ import { Outlet, Navigate } from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
 
+// Importando informações de contexto e autenticação de usuario
+import { AuthContext } from "../contexts/UserContext.jsx";
+import { useContext } from "react";
+
+// Importando Barra de Navegação
+// import NavBar from "../components/NavBar/NavBar.jsx";
+
 const RotasProtegidas = () => {
-    return(
-        <div></div>
-    )
-}
+    
+  // Pega a variável de usuario nome pra saber se tem alguém logado
+  const { usuarioNome } = useContext(AuthContext);
+
+  if (usuarioNome === "Visitante") {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <div>
+      {/* Campo para NavBar */}
+
+      {/* Container principal, dependendo em qual está */}
+      <div
+        className="d-flex flex-column min-vh-100 flex-grow-1 p-2 justify-content-center"
+        style={{ marginLeft: "350px" }}
+      >
+        <Container fluid>
+          <Outlet />
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 export default RotasProtegidas;
