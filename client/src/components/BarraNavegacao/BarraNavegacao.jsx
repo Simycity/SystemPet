4eb781a966cbbc1abc1cbfcf541ac5179531c0a2
Simycity <vhzfilho@hotmail.com>
2026-03-13 21:04:42 -0300

    adicionando NavBar

--- client/src/components/BarraNavegacao/BarraNavegacao.jsx
+++ client/src/components/BarraNavegacao/BarraNavegacao.jsx
@@ -4,13 +4,20 @@ import { Container, Nav, NavBar } from "react-bootstrap";
 const BarraNavegacao = () => {
     return(
         <div>
-            <Container>
-                <NavBar>
-                    <Nav></Nav>
-                </NavBar>
-            </Container>
+            <NavBar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Nav.Brand href="#home">NavBar</Nav.Brand>
+                    <Nav>
+                        <Nav.Link href="#home">Home</Nav.Link>    
+                        <Nav.Link href="#Cadastro">Cadastro</Nav.Link>    
+                        <Nav.Link href="#CardPet">CardPet</Nav.Link>    
+                        <Nav.Link href="#Clientes">Funcionarios</Nav.Link>    
+                        <Nav.Link href="#Funcionarios">Funcionarios</Nav.Link>    
+                    </Nav>  
+                </Container>
+            </NavBar>
         </div>
     );
 }
 
-export default BarraNavegacao();
+export default BarraNavegacao;
