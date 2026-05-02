667ced0b1ce3b74a8957f5c067d554e96bf8df07
Simycity <vhzfilho@hotmail.com>
2026-05-02 02:04:07 -0300

    Atualizando arquivos

--- client/src/MyRouter.jsx
+++ client/src/MyRouter.jsx
@@ -13,19 +13,16 @@ import Cadastro from "./pages/Cadastro/Cadastro.jsx";
 import Home from "./pages/Home/Home.jsx";
 
 // Importação das páginas de Funcionários
-import Funcionarios from "./pages/Funcionários/Funcionarios.jsx";
-import VerFuncionarios from "./pages/Funcionários/VerFuncionarios.jsx";
-import AtualizarFuncionarios from "./pages/Funcionários/AtualizarFuncionario/AtualizarFuncionario.jsx";
-import CadastarFuncionario from "./pages/Funcionários/CadastroFuncionario/CadastrarFuncionario.jsx";
+import VerFuncionarios from "./pages/Funcionarios/VerFuncionarios.js";
+import AtualizarFuncionarios from "./pages/Funcionarios/AtualizarFuncionario/AtualizarFuncionario.jsx";
+import CadastarFuncionario from "./pages/Funcionarios/CadastroFuncionario/CadastrarFuncionario.jsx";
 
-// Importação da págin DE Clientes
-import Clientes from "./pages/Clientes/Clientes.jsx";
+// Importação da págin de Clientes
 import VerClientes from "./pages/Clientes/VerCliente.jsx";
 import AtualizarClientes from "./pages/Clientes/AtualizarCliente/AtualizarCliente.jsx";
-import CastrarCliente from "./pages/Clientes/CadastrarCliente/CadastrarCliente.jsx";
+import CadastrarClientes from "./pages/Clientes/CadastrarCliente/CadastrarCliente.jsx";
 
 // Importando página de Pets
-import Pets from "./pages/Pets/Pets.jsx";
 import VerPet from "./pages/Pets/VerPet.jsx";
 import AtualizarPet from "./pages/Pets/AtualizarPet/AtualizarPets.jsx";
 import CadastrarPet from "./pages/Pets/CadastrarPet/CadastrarPets.jsx";
@@ -48,7 +45,49 @@ const router = createBrowserRouter([
     path: "/",
     element: <RotasProtegidas />,
     errorElement: <PaginaErro />,
-    children: [{ path: "home", element: <Home /> }],
+    children: [
+      { path: "/home", element: <Home /> },
+
+      // Rota de funcionários
+      {
+        path: "/funcionarios",
+        children: [
+          { index: true, element: <VerFuncionarios /> },
+          { index: "cadastrar", element: <CadastarFuncionario /> },
+          { index: "atualizar", element: <AtualizarFuncionarios /> },
+        ],
+      },
+
+      // Rota de clientes
+      {
+        path: "/clientes",
+        children: [
+          { index: true, element: <VerClientes /> },
+          { index: "cadastrar", element: <CadastrarClientes /> },
+          { index: "atualizar", element: <AtualizarClientes /> },
+        ],
+      },
+
+      // Rota de pets
+      {
+        path: "/pets",
+        children: [
+          { index: true, element: <VerPet /> },
+          { index: "cadastrar", element: <CadastrarPet /> },
+          { index: "atualizar", element: <AtualizarPet /> },
+        ],
+      },
+
+      
+
+
+
+
+
+
+
+
+    ],
   },
 ]);
 
